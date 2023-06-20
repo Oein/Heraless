cdb3eb497cdd7ba1b5a07eb033295906c7596d8d
Oein <oein0219@gmail.com>
2023-06-20 21:32:19 +0900

    rename to heraless

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { Next, Req, Res } from "../@types/bedrock";
+import { Req, Res } from "@/@types/heraless";
 
 export function GET(req: Req, res: Res) {
   res.send("This server is powered by Heraless by @github/Oein");
